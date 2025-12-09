a0eb6ac6cc2eb29ea1f48bb1f79eaee82901def8
Oscar Jomer <oscarllabres08@gmail.com>
2025-12-09 11:21:09 +0800

    Updated the facebook messenger username

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -2,5 +2,5 @@
 // Replace 'your-messenger-username' with your actual Facebook Messenger username
 // You can find your username at: https://www.facebook.com/llabresoscar08
 // Or use your Facebook Page username if you have a business page
-export const SELLER_MESSENGER_USERNAME = 'llabresoscar08';
+export const SELLER_MESSENGER_USERNAME = 'jerikodeguzman.lascano.14';
 
